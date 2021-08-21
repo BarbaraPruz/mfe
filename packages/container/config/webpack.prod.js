d703dae6f0c9b259567fd9dae46ee6e669ea9038
Barbara <barbara.prusiewicz@gmail.com>
2021-08-21 10:50:37 -1000

    Add container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const commonConfig = require('./webpack.common');
+// package json only needed for shared packages shortcut
+const packageJson = require('../package.json')
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${domain}/marketing`
+            },
+            // for explicit control of shared packages and versions, use shared
+            // shared: ['react', 'react-dom']
+            shared: packageJson.dependencies,
+        }),
+    ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
