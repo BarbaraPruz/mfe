53a18898b8df7ff79d446524fbc420d246a57095
Barbara <barbara.prusiewicz@gmail.com>
2021-09-27 15:07:11 -1000

    Added dashboard

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -18,7 +18,8 @@ const prodConfig = {
             name: 'container',
             remotes: {
                 marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                auth: `auth@${domain}/auth/latest/remoteEntry.js`
+                auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+                dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`
             },
             // for explicit control of shared packages and versions, use shared
             // shared: ['react', 'react-dom']
